cc319513530bfb4699831db88293611909746252
Jin Wang <wangjin@wangjin.me>
2019-06-26 17:49:25 -0500

    JA-29: Support render difference in login/non-login statuses

--- src/reducers/JDataReducer.js
+++ src/reducers/JDataReducer.js
@@ -1,5 +1,8 @@
+/* global localStorage: true */
 const initState = {
-    hideEverything: false
+    hideEverything: false,
+    loginStatus: typeof localStorage.getItem("jAssistantUserAuthKey") === "string",
+    userAuthKey: localStorage.getItem("jAssistantUserAuthKey") || null
 };
 
 const JDataReducer = (state = initState, action) => {
@@ -9,6 +12,23 @@ const JDataReducer = (state = initState, action) => {
             ...state,
             hideEverything: !state.hideEverything
         };
+        break;
+    case "UPDATE_LOGIN_STATUS":
+        if (action.newStatus) {
+            state = {
+                ...state,
+                loginStatus: true,
+                userAuthKey: action.newAuthKey
+            };
+            localStorage.setItem("jAssistantUserAuthKey", action.newAuthKey);
+        } else {
+            state = {
+                ...state,
+                loginStatus: false,
+                userAuthKey: null
+            };
+            localStorage.removeItem("jAssistantUserAuthKey");
+        }
         break;
     default:
         break;
